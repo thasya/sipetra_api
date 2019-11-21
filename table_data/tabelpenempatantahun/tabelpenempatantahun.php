e6337a2d4e596646536dcc6dec0e67bffc9afc80
thasya <thasya717@gmail.com>
2019-11-22 02:05:20 +0800

    Revisi

--- table_data/tabelpenempatantahun/tabelpenempatantahun.php
+++ table_data/tabelpenempatantahun/tabelpenempatantahun.php
@@ -1,5 +1,5 @@
 <?php
 	header('Access-Control-Allow-Origin: *');
-	$api_url = file_get_contents("tabelinventaris.json");
+	$api_url = file_get_contents("tabelpenempatantahun.json");
 	echo $api_url;
 ?>
